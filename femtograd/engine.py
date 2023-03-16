7d8411a96d02f77e6de1304d73d6f300f109f36d
Farouk Hamadi <faroukhamadi1@outlook.com>
2023-03-16 23:41:32 +0100

    refactor

--- femtograd/engine.py
+++ femtograd/engine.py
@@ -1,16 +1,32 @@
 class Node:
     def __init__(self, val) -> None:
         self.val = val
+        self.children = set()
+        self.gradient = float('inf')
 
     def __add__(self, other):
-        return self.val + other.val
+        res = self.val + other.val
+        backward = 2
+
+        self.children.add(other)
+        return res
 
     def __mul__(self, other):
-        return self.val * other.val
+        res = self.val * other.val
+        self.children.add(other)
+        return res
+
+    def __pow__(self, other):
+        res = self.val ** other.val
+        self.children.add(other)
+        return res
+
+    def __repr__(self) -> str:
+        return f'val={self.val}'
 
     def gradient(self, func):
         h = 0.0001
         return (func(self.val + h) - func(self.val)) / h
 
-    def __repr__(self) -> str:
-        return 'hello'
+    def relu(self):
+        return max(0, self.val)
