711c69717947b2391658004676da0f24d2116f0c
Farouk Hamadi <faroukhamadi1@outlook.com>
2023-03-19 20:10:43 +0100

    add pow

--- femtograd/engine.py
+++ femtograd/engine.py
@@ -1,19 +1,31 @@
+import math
+
+
 class Node:
     def __init__(self, val) -> None:
         self.val = val
         self.gradient = 0
         self.backward = lambda: None
 
-
     def __add__(self, other):
         res = Node(self.val + other.val)
-        
+
         def _backward():
             self.gradient = res.gradient
             other.gradient = res.gradient
-            
+
         res.backward = _backward
-        
+
+        return res
+
+    def __add__(self, other):
+        res = Node(self.val ** other.val)
+
+        def _backward():
+            self.gradient = (other * self.val**(other-1)) * res.gradient
+
+        res.backward = _backward
+
         return res
 
     def __mul__(self, other):
@@ -22,10 +34,17 @@ class Node:
         def _backward():
             self.gradient = other.val * res.gradient
             other.gradient = self.val * res.gradient
-            
+
         res.backward = _backward
-        
+
         return res
+
+    def __repr__(self) -> str:
+        return f'val={self.val}'
+
+    def _gradientDemo(self, func):
+        h = 0.0001
+        return (func(self.val + h) - func(self.val)) / h
 
     def tanh(self):
         res = Node((math.exp(2*self.val) - 1) / (math.exp(2*self.val) + 1))
@@ -36,18 +55,11 @@ class Node:
         res.backward = _backward
         return res
 
-    def __repr__(self) -> str:
-        return f'val={self.val}'
-
-    def _gradientDemo(self, func):
-        h = 0.0001
-        return (func(self.val + h) - func(self.val)) / h
-
     def relu(self):
         res = Node(max(0, self.val))
-        
+
         def _backward():
             self.gradient = 1 if res.val > 0 else 0
-        
+
         res.backward = _backward
         return res
